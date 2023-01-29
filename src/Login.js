38abe535dcb3a0865bd39883596b5d90333c1103
Nathan Chiu <115620289+nathanchiu888@users.noreply.github.com>
2023-01-29 14:13:51 -0800

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+
+export const Login = (props) => {
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="Your Username" id="username" name="username" />
+                <button type="submit" >Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
