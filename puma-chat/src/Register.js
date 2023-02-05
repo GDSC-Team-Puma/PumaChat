c6b4f48a05ad881fdc7b8bc3e8888ea4d4f1fc35
gabrielyap <glyap@ucdavis.edu>
2023-02-05 14:23:02 -0800

    fixed submit button in register.js line 18

--- puma-chat/src/Register.js
+++ puma-chat/src/Register.js
@@ -15,7 +15,7 @@ export const Register = (props) => {
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="username">Username</label>
             <input value={username} onChange={(e) => setUsername(e.target.value)}type="username" placeholder="Your Username" id="username" name="username" />
-            <button type="submit" onClick={console.log("submit")}>Log In</button>
+            <button type="submit" onClick={() => console.log(username)}>Log In</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
