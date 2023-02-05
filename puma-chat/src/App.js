7c9c22a7e03d3a70d97fad0d85de75f9e2d69d08
dan-medina <dmedinamail@gmail.com>
2023-02-05 14:12:23 -0800

    moved firebase initialization to index.js

--- puma-chat/src/App.js
+++ puma-chat/src/App.js
@@ -3,23 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./Login";
 import { Register } from "./Register";
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA8EN-MCSdx2aPaqi6Vj8gUdznOsB5t72A",
-  authDomain: "chatmessenger-66568.firebaseapp.com",
-  databaseURL: "https://chatmessenger-66568-default-rtdb.firebaseio.com",
-  projectId: "chatmessenger-66568",
-  storageBucket: "chatmessenger-66568.appspot.com",
-  messagingSenderId: "921628900891",
-  appId: "1:921628900891:web:5e60c649cefa499ce9c2be",
-  measurementId: "G-2HHWT1SZ18",
-  databaseURL: "https://chatmessenger-66568-default-rtdb.firebaseio.com"
-}
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
--- puma-chat/src/index.js
+++ puma-chat/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8EN-MCSdx2aPaqi6Vj8gUdznOsB5t72A",
+  authDomain: "chatmessenger-66568.firebaseapp.com",
+  databaseURL: "https://chatmessenger-66568-default-rtdb.firebaseio.com",
+  projectId: "chatmessenger-66568",
+  storageBucket: "chatmessenger-66568.appspot.com",
+  messagingSenderId: "921628900891",
+  appId: "1:921628900891:web:5e60c649cefa499ce9c2be",
+  measurementId: "G-2HHWT1SZ18",
+  databaseURL: "https://chatmessenger-66568-default-rtdb.firebaseio.com"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
