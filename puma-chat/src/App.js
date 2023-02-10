b92bcec97881d3316994e8ae5bc8eaf30a972d93
gabrielyap <glyap@ucdavis.edu>
2023-02-09 22:31:24 -0800

    Added Homepage.js and imported to App.js

--- puma-chat/src/App.js
+++ puma-chat/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./Login";
 import { Register } from "./Register";
+import { Homepage } from "./Homepage";
 
 function App() {
   const [currentForm, setCurrentForm] = useState("login");
---
+++ puma-chat/src/Homepage.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export const Homepage = (props) => {
+    return(
+        <h1> "HELLO WORLD"</h1>
+
+    )
+}
