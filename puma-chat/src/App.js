22c811d7beca0641e494fa834e1fee66f901c99e
gabrielyap <glyap@ucdavis.edu>
2023-02-18 23:22:26 -0800

    commiting just to sync 2_18

--- puma-chat/src/App.js
+++ puma-chat/src/App.js
@@ -1,25 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./Login";
 import { Register } from "./Register";
 import { Homepage } from "./Homepage";
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState("login");
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  };
+  const UserAuth = () => {
+    const [user, setUser] = useState(null);
+  
+    useEffect(() => {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          // User is signed in.
+          setUser(user);
+        } else {
+          // No user is signed in.
+          setUser(null);
+        }
+      });
+    }, []);
+  
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    };
+  
+    const signOut = () => {
+      firebase.auth().signOut();
+    };
 
   return (
     <div className="App">
-      {currentForm === "login" ? (
-        <Login onFormSwitch={toggleForm} />
+      {user ? (
+        <div>
+          <p>Welcome, {user.displayName}!</p>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
       ) : (
-        <Register onFormSwitch={toggleForm} />
+        <button onClick={signInWithGoogle}>Sign In with Google</button>
       )}
     </div>
   );
+      };
 }
 export default App;
