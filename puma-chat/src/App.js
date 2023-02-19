1b82a678e9c46856ece85dba8e1cca6645362e7c
gabrielyap <glyap@ucdavis.edu>
2023-02-18 23:38:16 -0800

    fixed repository to daniel's 2_16 commit

--- puma-chat/src/App.js
+++ puma-chat/src/App.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import logo from "./logo.svg";
-import "./App.css";
-import { Login } from "./Login";
-import { Register } from "./Register";
-import { Homepage } from "./Homepage";
-import firebase from 'firebase/compat/app';
-import 'firebase/auth';
-
-function App() {
-  const UserAuth = () => {
-    const [user, setUser] = useState(null);
-  
-    useEffect(() => {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          // User is signed in.
-          setUser(user);
-        } else {
-          // No user is signed in.
-          setUser(null);
-        }
-      });
-    }, []);
-  
-    const signInWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider);
-    };
-  
-    const signOut = () => {
-      firebase.auth().signOut();
-    };
-
-  return (
-    <div className="App">
-      {user ? (
-        <div>
-          <p>Welcome, {user.displayName}!</p>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
-      ) : (
-        <button onClick={signInWithGoogle}>Sign In with Google</button>
-=======
 import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./App.css";
@@ -62,10 +17,8 @@ function App() {
         <>
           <Homepage />
         </>
->>>>>>> 4db913c7417846ba46b139285b7832022b3202c5
       )}
     </div>
   );
-      };
 }
 export default App;
--- puma-chat/src/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export const Homepage = (props) => {
-    return(
-        <h1> "HELLO WORLD"</h1>
-        
-
-    )
-}
