2cf5cf1f1907b8c22836f940bf1eb267001c9f5f
dan-medina <dmedinamail@gmail.com>
2023-02-25 19:51:48 -0800

    initialized a ChatBox component

---
+++ puma-chat/src/components/ChatBox.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+
+const ChatBox = () => {
+}
+
+export default ChatBox;
